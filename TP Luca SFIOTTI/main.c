fc7c2aa41a14fadf30973458dd5b25260d881384
Luca Sfiotti <sfiottiluca@gmail.com>
2025-01-18 16:52:22 +0100

    etape 1

--- TP Luca SFIOTTI/main.c
+++ TP Luca SFIOTTI/main.c
@@ -1,28 +1,14 @@
 #include <stdio.h>
 
-void min_max(int tab[], int size, int *min, int *max) {
-
-	if (size < 1)
-	{
-		return;
-	}
-		*min = tab[0];
-		*max = tab[0];
-		for (int i = 0; i < size; i++)
-		{
-			if (*min > tab[i]) {
-				*min = tab[i];
-			}
-			if (*max < tab[i]) {
-				*max = tab[i];
-			}
-		}
-	}
 int main() {
-	int min = 0;
-	int max = 0;
-	int tableau[] = { 5, 21, 9999, -2, 1, 156, 8, 10};
-	min_max(tableau, 8, &min, &max);
-	printf("min = %d\nmax = %d", min, max);
-	return 0;
+	typedef struct {
+		const char* option;
+	}Menu;
+
+	typedef struct {
+		const char* nom;
+		int nb_menus;
+		Menu* menus;
+	}Application;
+
 }
