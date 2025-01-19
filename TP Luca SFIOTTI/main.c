513af51d61e12483eccfab865051f31a1376cdb0
Luca Sfiotti <sfiottiluca@gmail.com>
2025-01-19 17:35:54 +0100

    etape 2

--- TP Luca SFIOTTI/main.c
+++ TP Luca SFIOTTI/main.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 
+typedef struct Menu{
+	const char* option;
+}Menu;
+
+typedef struct Application{
+	const char* nom;
+	int nb_menus;
+	Menu *menus;
+}Application;
+
+void displayMenu(const Application* app) {
+	printf("--%s--\n", app->nom);
+
+	for (int i = 0; i < app->nb_menus; i++) {
+		printf("%d.%s\n", i + 1, app->menus[i].option);
+	};
+}
+
 int main() {
-	typedef struct {
-		const char* option;
-	}Menu;
+	Menu photoMenus[] = { {"Regarder une photo"}, { "Prendre une photo" } };
+	Application photoApp = { {"Photos"}, {"2"}, {photoMenus} };
 
-	typedef struct {
-		const char* nom;
-		int nb_menus;
-		Menu* menus;
-	}Application;
+	displayMenu(&photoApp);
 
+	return 0;
 }
