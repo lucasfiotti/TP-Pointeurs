fb3329022dc65f5e2351f4efc9efdcf5a2232d6c
Luca Sfiotti <sfiottiluca@gmail.com>
2025-01-20 08:00:42 +0100

    ce que j'ai fait pour l'étape 3, jensuis bloqué pour l'instant mais le rendu se rapproche de ce qui est demandé (c'est bof mais ça va)

--- TP Luca SFIOTTI/main.c
+++ TP Luca SFIOTTI/main.c
@@ -10,19 +10,44 @@ typedef struct Application{
 	Menu *menus;
 }Application;
 
-void displayMenu(const Application* app) {
-	printf("--%s--\n", app->nom);
 
-	for (int i = 0; i < app->nb_menus; i++) {
-		printf("%d.%s\n", i + 1, app->menus[i].option);
+
+void runApplication(const Application* app) {
+		int i = 0;
+		int choixU = 0;
+		printf("Choisissez une option (0 pour quitter )");
+		scanf_s("%d", &choixU);
+		if (choixU == 0) {
+			exit();
+		}
+		else if (choixU == 1) {
+			printf("Vous avez choisi : %d.%s", i + 1, (*app).menus[i].option);
+		}
+		else if (choixU == 2) {
+			printf("Vous avez choisi : %d.%s", i + 2, (*app).menus[i].option);
+		}
+		else if (choixU != 0 && choixU != 1 && choixU != 2) {
+			printf("Choix invalide, veuillez réessayer");
+		}
+}
+
+void displayMenu(const Application* app) {
+	printf("--%s--\n", (*app).nom);
+
+	for (int i = 0; i < (*app).nb_menus; i++) {
+		printf("%d.%s\n", i + 1, (*app).menus[i].option);
+		runApplication(app);
 	};
 }
+
+
 
 int main() {
 	Menu photoMenus[] = { {"Regarder une photo"}, { "Prendre une photo" } };
 	Application photoApp = { {"Photos"}, {"2"}, {photoMenus} };
 
 	displayMenu(&photoApp);
+	runApplication(&photoApp);
 
 	return 0;
 }
